7ada1229759d53883d74018bdaa79d12d6c89829
Eonist <30n1st@gmail.com>
2019-10-20 17:17:45 +0200

    Make smaller

--- src/ZoomSwitcherButton/ZoomSwitcherButton+Const.swift
+++ src/ZoomSwitcherButton/ZoomSwitcherButton+Const.swift
@@ -3,9 +3,9 @@ import Foundation
  * Const
  */
 extension ZoomSwitcherButton {
-   public static let spaceBetween: CGFloat = 12
-   public static let margin: CGFloat = 8
-   public static let buttonWidth: CGFloat = 42
+   public static let spaceBetween: CGFloat = 6
+   public static let margin: CGFloat = 6
+   public static let buttonWidth: CGFloat = 32
    public typealias Style = (textColor: UIColor, backgroundColor: UIColor)
    public static let on: Style = (backgroundColor: .white, textColor: .darkGray)
    public static let off: Style = (backgroundColor: .gray, textColor: .white)
